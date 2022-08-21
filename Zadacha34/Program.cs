a766fbbba07d9842944edab5146d623777cf9618
LEXASPB78 <ashkreba@yandex.ru>
2022-08-21 14:38:32 +0300

    Reshenie34

---
+++ Zadacha34/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] ArrayCreation(int arrayLengh)
+{
+    int[] array = new int[arrayLengh];
+    for (int i = 0; i < arrayLengh; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void NumberOfEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    Console.Write($"Количество четных членов массива = {count}");
+}
+
+Console.Write("Введите длину желаемого массива ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int[] myArray = ArrayCreation(arrayLength);
+PrintArray(myArray);
+NumberOfEven(myArray);
